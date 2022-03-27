c683286c7773f8f1c5418a570801ecab50da4cde
ShahriarAhmod <ashahriyar71@gmail.com>
2022-03-27 21:30:01 +0600

    3rd

---
+++ src/Videos.js
@@ -0,0 +1,106 @@
+import React from 'react'
+import Filter from './Filter';
+import SingleVideo from './SingleVideo';
+import "./Videos.css";
+
+function Videos() {
+
+    return (
+        <div className="videos">
+            <Filter />
+            {/* Videos */}
+              <div className="row">
+                  <SingleVideo 
+                  thumb__img="https://i.ytimg.com/vi/hQjlM-8C4Ps/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBN6vr7kiELWRyfHkHQLsg8ydLDCQ"
+                  profile__img="https://yt3.ggpht.com/ytc/AKedOLR-TP_Uc-gh9UWENj1CsWNVyxDRwCikaVARVwhY=s68-c-k-c0x00ffffff-no-rj"
+                  title="React Portfolio Website | Build and Deploy |..."
+                  chaneel__name="Lama Dev"
+                  views="21k views ."
+                  timestamps=" 1 week ago"
+                  videos__duration="1:39:53"
+                  />
+                  <SingleVideo 
+                  thumb__img="https://i.ytimg.com/vi/FAWch-wOs4I/hqdefault.jpg?sqp=-oaymwEXCOADEI4CSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLCwtXnjErrVJDqoOPK8dqyBg6HxZQ"
+                  profile__img="https://yt3.ggpht.com/ytc/AKedOLQICsLr5jSdgCCZo2RjCtsQ6H2c3SOkuXHI1YZDfw=s68-c-k-c0x00ffffff-no-rj"
+                  title="Neela Miles | guitar cover ..."
+                  chaneel__name="Miles"
+                  views="21k views ."
+                  timestamps=" 1 week ago"
+                  videos__duration="04:32"
+                  />
+                  <SingleVideo 
+                  thumb__img="https://i.ytimg.com/vi/Po7yLhlBudg/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAhQRfBTv7am-r3S4oUstEpjHx6pQ"
+                  profile__img="https://yt3.ggpht.com/3wUoiwrTI1S4wd4vKZYFsHzuAJCVs5n_vC4O1qjDV7_AZ78bkvtWo4VfNSBi-6Co-qzOeSWHfw=s68-c-k-c0x00ffffff-no-rj"
+                  title="#16 JavaScript Bangla tutorial | Javascript..."
+                  chaneel__name="Learn With Sumit"
+                  views="4.2k views ."
+                  timestamps=" 2 months ago"
+                  videos__duration="33:53"
+                  />
+                  <SingleVideo 
+                  thumb__img="https://i.ytimg.com/vi/gIHjLTmKHgM/hqdefault.jpg?sqp=-oaymwEXCOADEI4CSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLDlgMdaNf0D8R_Mk_PaLZKqzdzF_Q"
+                  profile__img="https://yt3.ggpht.com/ytc/AKedOLQy6MJvKgmtlQRk137FP1rZtMo8dyo3FTfDBG4d=s68-c-k-c0x00ffffff-no-rj"
+                  title="Chuye dilm chuye dile mon Movie song"
+                  chaneel__name="Bd Songs"
+                  views="42k views ."
+                  timestamps=" 1 week ago"
+                  videos__duration="04:41"
+                  />
+
+
+                  <SingleVideo 
+                  thumb__img="https://i.ytimg.com/vi/gIHjLTmKHgM/hqdefault.jpg?sqp=-oaymwEXCOADEI4CSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLDlgMdaNf0D8R_Mk_PaLZKqzdzF_Q"
+                  profile__img="https://yt3.ggpht.com/ytc/AKedOLQy6MJvKgmtlQRk137FP1rZtMo8dyo3FTfDBG4d=s68-c-k-c0x00ffffff-no-rj"
+                  title="Chuye dilm chuye dile mon Movie song"
+                  chaneel__name="Bd Songs"
+                  views="42k views ."
+                  timestamps=" 1 week ago"
+                  videos__duration="04:41"
+                  />
+
+                  <SingleVideo 
+                  thumb__img="https://i.ytimg.com/vi/Po7yLhlBudg/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAhQRfBTv7am-r3S4oUstEpjHx6pQ"
+                  profile__img="https://yt3.ggpht.com/3wUoiwrTI1S4wd4vKZYFsHzuAJCVs5n_vC4O1qjDV7_AZ78bkvtWo4VfNSBi-6Co-qzOeSWHfw=s68-c-k-c0x00ffffff-no-rj"
+                  title="#16 JavaScript Bangla tutorial | Javascript..."
+                  chaneel__name="Learn With Sumit"
+                  views="4.2k views ."
+                  timestamps=" 2 months ago"
+                  videos__duration="33:53"
+                  />
+
+                  <SingleVideo 
+                  thumb__img="https://i.ytimg.com/vi/FAWch-wOs4I/hqdefault.jpg?sqp=-oaymwEXCOADEI4CSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLCwtXnjErrVJDqoOPK8dqyBg6HxZQ"
+                  profile__img="https://yt3.ggpht.com/ytc/AKedOLQICsLr5jSdgCCZo2RjCtsQ6H2c3SOkuXHI1YZDfw=s68-c-k-c0x00ffffff-no-rj"
+                  title="Neela Miles | guitar cover ..."
+                  chaneel__name="Miles"
+                  views="21k views ."
+                  timestamps=" 1 week ago"
+                  videos__duration="04:32"
+                  />
+
+                <SingleVideo 
+                  thumb__img="https://i.ytimg.com/vi/hQjlM-8C4Ps/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBN6vr7kiELWRyfHkHQLsg8ydLDCQ"
+                  profile__img="https://yt3.ggpht.com/ytc/AKedOLR-TP_Uc-gh9UWENj1CsWNVyxDRwCikaVARVwhY=s68-c-k-c0x00ffffff-no-rj"
+                  title="React Portfolio Website | Build and Deploy |..."
+                  chaneel__name="Lama Dev"
+                  views="21k views ."
+                  timestamps=" 1 week ago"
+                  videos__duration="1:39:53"
+                  />
+
+                <SingleVideo 
+                  thumb__img="https://i.ytimg.com/vi/FAWch-wOs4I/hqdefault.jpg?sqp=-oaymwEXCOADEI4CSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLCwtXnjErrVJDqoOPK8dqyBg6HxZQ"
+                  profile__img="https://yt3.ggpht.com/ytc/AKedOLQICsLr5jSdgCCZo2RjCtsQ6H2c3SOkuXHI1YZDfw=s68-c-k-c0x00ffffff-no-rj"
+                  title="Neela Miles | guitar cover ..."
+                  chaneel__name="Miles"
+                  views="21k views ."
+                  timestamps=" 1 week ago"
+                  videos__duration="04:32"
+                  />
+
+              </div>
+        </div>
+    )
+}
+
+export default Videos
